4f770caf5c8abbcf98a5db257c875085a751036e
antelor <ramiro.antelo4@gmail.com>
2021-06-13 17:15:11 -0300

    html y css basico agregado

---
+++ script.js
@@ -0,0 +1,22 @@
+function add (a, b) {
+    return a + b;
+}
+
+function substract (a, b) {
+    return a-b;
+}
+
+function multiply (a, b) {
+    return a*b;
+}
+
+function divide (a, b){
+    return a/b;
+}
+
+function operate (op, a, b){
+    if (op == '+') return add(a,b);
+    if (op == '-') return substract(a,b);
+    if (op == '*') return multiply(a,b);
+    if (op == '/') return divide(a,b);
+}
