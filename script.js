bfa0830da712aec9b2e5f83e5660cbb4082e4c58
antelor <ramiro.antelo4@gmail.com>
2021-06-13 19:01:38 -0300

    operaciones basicas con enteros funcionando

--- script.js
+++ script.js
@@ -20,3 +20,69 @@ function operate (op, a, b){
     if (op == '*') return multiply(a,b);
     if (op == '/') return divide(a,b);
 }
+
+function screenUpdate (){
+    screen.textContent = screenValue;
+}
+
+function numBtnClick(e){
+    let num = parseInt( e.target.textContent );
+    screenValue = screenValue * 10 + num;
+    
+    screenUpdate();
+}
+
+function opBtnClick(e){
+    currentOperator = e.target.textContent;
+
+    if ( (currentOperator=='*'||currentOperator=='/') && screenValue==0 ) {
+        console.log('b');
+        screenValue = operate( currentOperator, oldValue, 1);
+    }
+    else if ( (currentOperator=='*'||currentOperator=='/') && oldValue==0 ) {
+        screenValue = screenValue;
+    }
+    else if ( (currentOperator=='*'||currentOperator=='/') && (oldValue==0&&screenValue==0)) {
+        screenValue = 0;
+    }
+    else {
+        screenValue = operate( currentOperator, oldValue, screenValue);
+    }
+
+    screenUpdate();
+
+    oldValue = screenValue;
+    screenValue = 0;
+}
+
+function clearBtnClick(e){
+    oldValue = 0;
+    screenValue = 0;
+    screenUpdate();
+}
+
+function equalBtnClick(e){
+    screenValue = operate( currentOperator, oldValue, screenValue);
+    screenUpdate();
+
+    oldValue = screenValue;
+    screenValue = 0;
+}
+
+
+const screen = document.querySelector('[class=pantalla]');
+let screenValue = 0;
+let oldValue = 0;
+let currentOperator = '';
+
+document.querySelectorAll('.btn.numBtn').forEach(item => {
+    item.addEventListener('click', numBtnClick)
+});
+
+document.querySelectorAll('.btn.opBtn').forEach(item => {
+    item.addEventListener('click', opBtnClick)
+});
+  
+document.querySelector('.clearBtn').addEventListener('click', clearBtnClick);
+
+document.querySelector('.equalBtn').addEventListener('click', equalBtnClick);
